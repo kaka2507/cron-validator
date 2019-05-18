26f846db8a9ac65f63faf82b0b5b82509419cfaf
vcoder <doanngocbao@gmail.com>
2019-05-18 15:39:38 +0700

    Upd: release 1.0.1

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='cron-validator',
     packages=['cron_validator'],
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description='Unix cron implementation by Python',
     author='vcoder',
     author_email='doanngocbao@gmail.com',
     url='https://github.com/vcoder4c/cron-validator',
     keywords=['cron', 'python', 'cron expression validator', 'cron expression iterator'],
-    download_url='https://github.com/vcoder4c/cron-validator/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/vcoder4c/cron-validator/archive/v1.0.1.tar.gz',
     install_requires=[
         'python_dateutil',
         'pytz'
