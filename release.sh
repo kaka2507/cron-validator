f2e6402ea637bf355f8ada542e89e767826d09e9
Bao Doan N <doanngocbao@gmail.com>
2020-09-21 10:35:57 +0700

    Upd: correct code for release (https://github.com/pypa/warehouse/issues/5890)

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pip install -r requirements/release.txt
 python setup.py sdist
+python setup.py bdist_wheel
 twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from distutils.core import setup
+from setuptools import setup
 
 if sys.version_info[0] < 3:
     with open('README.md') as f:
